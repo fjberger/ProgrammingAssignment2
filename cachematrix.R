30f34263d57e2e95a49a6b43cdcda12f01923d4d
fjberger <post@florianberger.net>
2015-07-22 21:06:16 +0200

    update to the R file for the programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix takes argument x, created its inverse and generates a list with the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        #inverse is calculated
+        inv_x<<-solve(x)
+        #list containing matrix and its inverse is generated
+        list_mat_inv<<-list(matrix=x, inv=inv_x)
 }
 
 
@@ -12,4 +15,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+        # m is set to the inverse contained in the list 
+        m<- list$inv
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        matrix <- list$matrix()
+        m <- solve(data)
+        }
